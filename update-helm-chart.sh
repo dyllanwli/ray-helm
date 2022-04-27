50e64757e1bd9d5f399e5d028c7740775edd4b68
dyllanwli <messsking@gmail.com>
2022-04-27 16:39:26 -0500

    update helm chart

---
+++ update-helm-chart.sh
@@ -0,0 +1,10 @@
+
+
+mkdir tmp; cd tmp; git clone https://github.com/ray-project/ray.git
+cd ..
+mv ./tmp/ray/deploy/charts/ray ./
+rm -rf ./tmp
+
+git add .
+git commit -m "update helm chart"
+git push
